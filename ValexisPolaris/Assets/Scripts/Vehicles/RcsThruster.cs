ca626610be98dd8456885d9b4c8a5561e9c7f703
PederHundeboel <imtruxa@gmail.com>
2022-05-23 13:42:51 +0200

    Modified gitignore
    Modified gitignore to actually ignore unity meta files :)
    
    Was previously located outside the root folder xd

--- ValexisPolaris/Assets/Scripts/Vehicles/RcsThruster.cs
+++ ValexisPolaris/Assets/Scripts/Vehicles/RcsThruster.cs
@@ -7,8 +7,6 @@ public class RcsThruster : RocketEngine
     // Start is called before the first frame update
     void Start()
     {
-        //Debug.Log(gameObject.name + transform.localPosition + "\n tr local fwd = " + (transform.InverseTransformDirection(transform.transform.forward)));
-        //Debug.Log(gameObject.name + transform.localPosition + "\n tr local fwd = " + transform.forward + "VS. : " + (Quaternion.Euler(transform.localEulerAngles) * transform.InverseTransformDirection(transform.forward)) + "VS. : " + (transform.InverseTransformDirection(Vector3.forward)) + "world ");
         Debug.Log(gameObject.name + transform.localPosition + "\n tr local fwd = " + (Quaternion.Euler(transform.localEulerAngles) * transform.InverseTransformDirection(transform.forward)));
 
     }
